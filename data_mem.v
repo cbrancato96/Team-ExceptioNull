38afb8fe0d30788c014e3c16ece70ec42ea8a5e4
Chris Brancato <crbrancat@gmail.com>
2016-04-26 13:35:00 -0400

    Update data_mem.v

--- data_mem.v
+++ data_mem.v
@@ -1,21 +1,25 @@
+// Code your design here
 module data_mem () ;
 
 input [5:0] data_address; //data RAM depth is 64 lines
 input [7:0] write_data; 
 input write_enable;
-ouput [7:0] read_data;
+output [7:0] read_data;
 
 reg [5:0] data_address;
 reg [7:0] write_data;
-reg [7:0] mem[4:0];
-
+reg [7:0] mem [4:0];
+wire [7:0] read_data;
+  
 always @ (write_enable) begin
-  if (write_enable)
+ 	 if (write_enable)
     mem[data_address] <= write_data;
     read_data <= write_data;
-  else begin
+	end
+else 
+    
+begin
     read_data <= mem[data_address];
 end
-
+  end 
 endmodule
-
