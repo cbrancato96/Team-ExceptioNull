285a6dd9409b21df303d15c8efd3a6e6141e1a15
Chris Brancato <crbrancat@gmail.com>
2016-04-26 15:35:47 -0400

    Update instuction_mem.v

--- instuction_mem.v
+++ instuction_mem.v
@@ -1,13 +1,34 @@
-module instruction_mem ();
-
-reg [7:0] instruction_address;
-reg [7:0] instruction_data;
+module instruction_mem (
+instruction_address,
+instruction_data,
+);
 
 input [7:0] instruction_address;
-output [7:0] instruction_data;
+output [7:0] instruction_data;  
 
-assign
-instruction_address <= instuction_data;
-end
+reg [7:0] instruction_data;
+reg [7:0] instruction_mem [7:0];
+  
+assign instruction_data = instruction_mem[instruction_address];
+  
+endmodule
+
+
+// Code your testbench here
+// or browse Examples
+
+module test();
+  reg [7:0] instruction_address;
+  wire [7:0] instruction_data;
+  
+  instruction_mem INSTRUCTION_MEM(
+    .instruction_address(instruction_address),
+    .instruction_data(instruction_data));
+ 
+  initial begin
+    $display("instruction_address=%b,instruction_data=%b",instruction_address, instruction_data);
+
+$monitor("instruction_address=%b,instruction_data=%b",instruction_address, instruction_data);
+  end
 
 endmodule
