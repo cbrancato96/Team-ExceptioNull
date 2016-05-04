12d62aaac95df445e20498e80f36fecd6bbc4c0a
Chris Brancato <crbrancat@gmail.com>
2016-05-04 16:41:06 -0400

    Update control_unit.v

--- control_unit.v
+++ control_unit.v
@@ -1,184 +1,188 @@
 // Code your design here
 module control_unit(
   instruction,
-  op,
-  alu_control,
-  alu_zero,
-  branch,
-  alu_src,
-  wren_reg,
-  wren_data,
-  datamem_toreg);
+  jump,
+  mem_w_en,
+  reg_w_en,
+  sel_w_source);
 
   input [7:0] instruction;
-  inout [3:0] op;
+ 
+  output [7:0] jump;
+  output mem_w_en;
+  output reg_w_en;
+  output sel_w_source;
   
-  wire [3:0] op;
+  wire [3:0] opcode;
   wire [7:0] instruction;
   
-  output alu_zero;
-  output [2:0] alu_control;
-  output branch;
-  output alu_src;
-  output wren_reg;
-  output wren_data;
-  output datamem_toreg;
-  
-  reg [2:0] alu_control;
-  reg alu_zero;
-  reg branch;
-  reg alu_src;
-  reg wren_reg;
-  reg wren_data;
-  reg datamem_toreg;
+  reg [7:0] jump;
+  reg mem_w_en;
+  reg reg_w_en;
+  reg sel_w_source;
   
 
-  assign op = instruction[7:4];
+  assign opcode = instruction[7:4];
+  
   always @(instruction) 
     begin
       
-    if (op == 4'b0000) begin   //Move
-      alu_control = 3'b000;
-      branch = 0;
-      alu_src = 1;
-      wren_reg = 1;
-      wren_data = 0;
-      datamem_toreg = 1;
+    if (opcode == 4'b0000) begin   //Move
+      reg_addr_0 = instruction[1:0]; 
+      reg_addr_1 = instruction[3:2]; 
+      reg_addr_w = instruction[3:2]; 
+      jump = 8'b0;
+      mem_w_en = 0;
+      reg_w_en = 1;
+      sel_w_source = 0;
       end else
         
     if (op == 4'b0001) begin   //Add
-      alu_control = 3'b001;
-      branch = 0;
-      alu_src = 1;
-      wren_reg = 1;
-      wren_data = 0;
-      datamem_toreg = 1;
+      reg_addr_0 = instruction[1:0]; 
+      reg_addr_1 = instruction[3:2]; 
+      reg_addr_w = 2'b0; 
+      jump = 8'b0;
+      mem_w_en = 0;
+      reg_w_en = 1;
+      sel_w_source = 0;
       end else 
         
     if (op == 4'b0010) begin   //And
-      alu_control = 3'b010;
-      branch = 0;
-      alu_src = 1;
-      wren_reg = 1;
-      wren_data = 0;
-      datamem_toreg = 1;
+      reg_addr_0 = instruction[1:0]; 
+      reg_addr_1 = instruction[3:2]; 
+      reg_addr_w = 2'b0; 
+      jump = 8'b0;
+      mem_w_en = 0;
+      reg_w_en = 1;
+      sel_w_source = 0;
       end else
         
     if (op == 4'b0011) begin//Not
-      alu_control = 3'b011;
-      branch = 0;
-      alu_src = 1;
-      wren_reg = 1;
-      wren_data = 0;
-      datamem_toreg = 1;
+      reg_addr_0 = instruction[1:0]; 
+      reg_addr_1 = instruction[3:2]; 
+      reg_addr_w = instruction[3:2]; 
+      jump = 8'b0;
+      mem_w_en = 0;
+      reg_w_en = 1;
+      sel_w_source = 0;
       end else
         
     if (op == 4'b0100) begin   //Nor
-      alu_control = 3'b100;
-      branch = 0;
-      alu_src = 1;
-      wren_reg = 1;
-      wren_data = 0;
-      datamem_toreg = 1;
+      reg_addr_0 = instruction[1:0]; 
+      reg_addr_1 = instruction[3:2]; 
+      reg_addr_w = 2'b0; 
+      jump = 8'b0;
+      mem_w_en = 0;
+      reg_w_en = 1;
+      sel_w_source = 0;
   	  end else 
         
-    if (op == 4'b0101) begin   //Slt: Set Less Than 
-      alu_control = 3'b101;
-      branch = 0;
-      alu_src = 1;
-      wren_reg = 1;
-      wren_data = 0;
-      datamem_toreg = 1;
+    if (op == 4'b0101) begin   //Slt: Set Less Than
+      reg_addr_0 = instruction[1:0]; 
+      reg_addr_1 = instruction[3:2]; 
+      reg_addr_w = 2'b0; 
+      jump = 8'b0;
+      mem_w_en = 0;
+      reg_w_en = 1;
+      sel_w_source = 0;
       end else
         
     if (op == 4'b0110) begin    //Sll: Shift Left Logical
-      alu_control = 3'b110;
-      branch = 0;
-      alu_src = 1;
-      wren_reg = 0;
-      wren_data = 0;
-      datamem_toreg = 1;
+      reg_addr_0 = instruction[1:0]; 
+      reg_addr_1 = instruction[3:2]; 
+      reg_addr_w = instruction[3:2]; 
+      jump = 8'b0;
+      mem_w_en = 0;
+      reg_w_en = 1;
+      sel_w_source = 0;
       end else
         
     if (op == 4'b0111) begin   //Srl: Shift Right Logical
-      alu_control = 3'b111;
-      branch = 0;
-      alu_src = 1;
-      wren_reg = 0;
-      wren_data = 0;
-      datamem_toreg = 1;
+      reg_addr_0 = instruction[1:0]; 
+      reg_addr_1 = instruction[3:2]; 
+      reg_addr_w = instruction[3:2]; 
+      jump = 8'b0;
+      mem_w_en = 0;
+      reg_w_en = 1;
+      sel_w_source = 0;
       end else 
     
     if (op == 4'b1000) begin   //J: Jump
-      alu_control = 3'b000;
-      branch = 1;
-      alu_src = 1;
-      wren_reg = 0;
-      wren_data = 0;
-      datamem_toreg = 0;
+      reg_addr_0 = instruction[1:0]; 
+      reg_addr_1 = instruction[3:2];
+      reg_addr_w = instruction[3:2]; 
+      jump = 8'b11111111;
+      mem_w_en = 0;
+      reg_w_en = 0;
+      sel_w_source = 0;
   	  end else 
         
     if (op == 4'b1001) begin    //Jal: Jump and link
-      alu_control = 3'b001;
-      branch = 1;
-      alu_src = 1;
-      wren_reg = 0;
-      wren_data = 0;
-      datamem_toreg = 0;
+      reg_addr_0 = instruction[1:0]; 
+      reg_addr_1 = instruction[3:2];
+      reg_addr_w = instruction[3:2];
+      jump = 8'b11111111;
+      mem_w_en = 0;
+      reg_w_en = 0;
+      sel_w_source = 0;
   	  end else
         
     if (op == 4'b1010) begin   //lw: load word
-      alu_control = 3'b010;
-      branch = 0;
-      alu_src = 1;
-      wren_reg = 0;
-      wren_data = 0;
-      datamem_toreg = 1;
+      reg_addr_0 = instruction[1:0]; 
+      reg_addr_1 = instruction[3:2];
+      reg_addr_w = 2'b0;
+      jump = 8'b0;
+      mem_w_en = 0;
+      reg_w_en = 1;
+      sel_w_source = 1;
       end else 
         
     if (op == 4'b1011) begin  //sw: store word
-      alu_control = 3'b011;
-      branch = 0;
-      alu_src = 0;
-      wren_reg = 1;
-      wren_data = 1;
-      datamem_toreg = 1;
+      reg_addr_0 = instruction[1:0]; 
+      reg_addr_1 = instruction[3:2];
+      reg_addr_w = 2'b0;
+      jump = 8'b0;
+      mem_w_en = 1;
+      reg_w_en = 0;
+      sel_w_source = 0;
       end else
         
     if (op == 4'b1100) begin   //beq: branch if equal
-      alu_control = 3'b100;
-      branch = 1;
-      alu_src = 1;
-      wren_reg = 0;
-      wren_data = 0;
-      datamem_toreg = 0;
+      reg_addr_0 = instruction[1:0]; 
+      reg_addr_1 = instruction[3:2];
+      reg_addr_2 = 2'b0;
+      reg_addr_w = 2'b0;
+      jump = 8'b11111111;
+      mem_w_en = 0;
+      reg_w_en = 0;
+      sel_w_source = 0;
       end else
         
     if (op == 4'b1101) begin   //bne: branch not equal
-      alu_control = 3'b101;
-      branch = 1;
-      alu_src = 1;
-      wren_reg = 0;
-      wren_data = 0;
-      datamem_toreg = 0;
+      reg_addr_0 = instruction[1:0]; 
+      reg_addr_1 = instruction[3:2];
+      reg_addr_w = 2'b0;
+      jump = 8'b11111111;
+      mem_w_en = 0;
+      reg_w_en = 0;
+      sel_w_source = 0;
       end else
         
     if (op == 4'b1110) begin  //addi: add immediate
-      alu_control = 3'b110;
-      branch = 0;
-      alu_src = 1;
-      wren_reg = 0;
-      wren_data = 0;
-      datamem_toreg = 0;
+      reg_addr_0 = instruction[1:0]; 
+      reg_addr_1 = instruction[3:2];
+      reg_addr_w = instruction[3:2];
+      jump = 8'b0;
+      mem_w_en = 0;
+      reg_w_en = 1;
+      sel_w_source = 0;
   	  end else 
         
     if (op == 4'b1111) begin //li: load immediate
-      alu_control = 3'b111;
-      branch = 0;
-      alu_src = 1;
-      wren_reg = 1;
-      wren_data = 0;
-      datamem_toreg = 1;
+      jump = 8'b0;
+      mem_w_en = 0;
+      reg_w_en = 1;
+      sel_w_source = 0;;
       end
     end   
     
