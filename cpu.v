46ea6db6db2b6c85845726f4799b0029ac44aca6
ihsangunay <igunay70@gmail.com>
2016-05-04 20:22:43 -0400

    Update cpu.v

--- cpu.v
+++ cpu.v
@@ -1,10 +1,21 @@
 module cpu()
  
   //Declarations
+  reg [2:0] state;
+  reg [2:0] state_update;
+  reg [7:0] instruction;
+  reg update_pc;
+  reg access_mem;
+  reg decode;
+  reg execute;
+  
+  wire [7:0] instruction_data;
   wire [7:0] pc;
-  wire [7:0] instruction;
+  wire [3:0] opcode;
   
   // Register File I/O
+  reg [7:0] reg_file [3:0]
+  
   wire [1:0] reg_addr_0; // Address of Source Reg 0
   wire [1:0] reg_addr_1; // Address of Source Reg 1
   wire [1:0] reg_addr_w; // Address of Destination Reg
@@ -22,16 +33,75 @@ module cpu()
   
   // Results
   wire [7:0] alu_result;
+  wire branch;
   wire overflow;
+  wire [7:0] jump_offset;
   wire [7:0] mem_r_result;
   
   // Assignments
-  always @(state == 2'b010)
-    begin
-        assign reg_data_0 = 
+  assign opcode instruction [7:4];
+  assign state_update = state + 1;
+  assign result = (alu_result & (~sel_w_source) + mem_r_result & sel_w_source)
+  
+  always @(state) begin
+    case (state)
+      3'b000: // Fetch Instruction
+        begin
+          instruction <= instuction_data;
+          state <= state_update; 
+        end
+      3'b001: // Decode Instruction
+        begin
+          decode <= 1'b1;
+          decode <= #1 0'b0;
+          state <= state_update;
+        end
+      3'b010: // Get Data From Registers
+        begin
+          assign reg_data_0 = reg_file[reg_addr_0];
+          assign reg_data_1 = reg_file[reg_addr_1];
+          if (opcode == 4'b1100 || opcode == 4'b1101) begin // beq or bne
+            assign jump_offset = reg_file[2'b00];
+          end
+          state <= state_update;
+        end
+      3'b011: // ALU
+        begin
+          execute <= 1'b1;
+          execute <= #1 1'b0;
+          state <= state_update;
+        end
+      3'b100: // Data Memory Access
+        begin
+          if (mem_r_en || mem_w_en) begin
+            access_mem <= 1'b1;
+            access_mem <= #1 1'b0;
+            end
+          state <= state_update;
+        end
+      3'b101: // Writeback Data Resolution
+        begin
+            reg_w_data <= (alu_result & (~sel_w_source) + mem_r_result & sel_w_source)
+          state <= state_update;
+        end
+      3'b110: // Writeback
+        begin
+          if (reg_w_en) begin
+            reg_file[reg_addr_w] <= reg_data_w;
+            end
+          state <= state_update;  
+        end
+      3'b111: // PC Update
+        begin
+          update_pc <= 1'b0;
+          update_pc <= #1 1'b1;
+          state <= state_update;
+        end
+      endcase
+    end  
  
   // Operation Instantiation
-  instruction_mem instMem (.instruction_address(pc), .instruction_data(instruction));
+  instruction_mem instMem (.instruction_address(pc), .instruction_data(instruction_data));
   
   control_unit ctrl (.instruction(instruction)
                      .jump(jump),
@@ -49,9 +119,17 @@ module cpu()
                    .out(alu_result)
                    .overflow(overflow));
   
-  data_mem dataMem (.data_address(data_address), .write_data(write_data), .write_enable(write_enab    le), read_data(read_data));
+  data_mem dataMem (.data_address(data_address),
+                    .write_data(write_data),
+                    .write_enable(write_enable),
+                    .read_data(read_data),
+                    .clk(access_mem));
   
-  program_counter pcounter (.pc_control(jump), .jump_offset(jump_offset), .pc(pc));
+  program_counter pcounter (.pc_control(jump),
+                            .jump_offset(jump_offset),
+                            .pc(pc)
+                            .clk(update_pc));
+                            
  // Display to Screen
  always @(posedge clk)
  begin
