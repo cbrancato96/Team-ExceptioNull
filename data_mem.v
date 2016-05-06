46d8e7c4865adfc69a87151e8fe7d7b2c65d79a7
krisht <krisht@cooper.edu>
2016-05-06 13:49:34 -0400

    Fixed errors with data_mem.v

--- data_mem.v
+++ data_mem.v
@@ -52,7 +52,7 @@ module test();
     $display("data_address=%b, write_data=%b, write_enable=%b, read_data=%b", data_address, write_data, write_enable, read_data);
     clk = 0;
 	write_enable = 8'b0;
-    data_address = 8'b0;
+    data_address = 8'b1000010;
 	write_data = 8'b1;
     $monitor("data_address=%b, write_data=%b, write_enable=%b, read_data=%b, clk=%b", data_address, write_data, write_enable, read_data, clk);
     write_enable = 8'b0;
