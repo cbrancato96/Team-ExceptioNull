42b5ce87d651e33f5c0972c06107642e5ab0fef6
Chris Brancato <crbrancat@gmail.com>
2016-05-07 20:02:05 -0400

    Update program_counter.v

--- program_counter.v
+++ program_counter.v
@@ -2,11 +2,13 @@ module program_counter(
   clk,
   pc_control,
   jump_offset,
-  pc);
+  pc,
+startup);
 
   input clk;
   input [7:0] pc_control;
   input [7:0] jump_offset;
+  input startup;
   wire [7:0] pc_update;
   output [7:0] pc;
   
@@ -14,43 +16,47 @@ module program_counter(
   wire [7:0] offset;
   
   assign pc_update = pc + 1 + (pc_control & jump_offset);
-  initial
-    begin
-      pc = 8'b0;
-    end
+
   
   always @ (posedge clk)
     begin 
-      if (clk) begin 
-  	    pc <= pc_update;
-      end
+      if (startup == 1) begin 
+        pc = 8'b0;
+    	end else
+          pc <= pc_update;
   end 
 endmodule
+
 /*
-// Testbench
+//Testbench
 module test;
   
   reg clk;
   reg [7:0] pc_control;
   reg [7:0] jump_offset;
-  wire [7:0] pc;
+  reg startup;
+  reg [7:0] pc;
   
    program_counter PROGRAM_COUNTER(
     .clk(clk),
     .pc_control(pc_control),
     .jump_offset(jump_offset),
-    .pc(pc));
+    .pc(pc),
+    .startup(startup));
     
   initial begin
     $display("Initial conditions");
+    startup = 1;
     clk = 0;
     pc_control = 8'b0;
     jump_offset = 8'b11;
     display;
     
+    #10 startup = 1;
     clk = 1;
     display;
     
+    #10 startup = 0;
     clk = 0;
     display;
     
@@ -69,9 +75,7 @@ module test;
  
   
  task display;
-   #1 $display("pc:%b, pc_control:%b, jump_offset=%b, clk=%b", pc, pc_control, jump_offset, clk);
+   #1 $display("pc:%b, pc_control:%b, jump_offset=%b, clk=%b, startup=%b", pc, pc_control, jump_offset, clk, startup);
   endtask
-  
-  
 endmodule
 */
